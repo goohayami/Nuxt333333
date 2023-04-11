68ca5006fb501394a32afa7546b475a7e9beff6c
hayami <goohayami@gmail.com>
2023-04-11 22:21:04 +0900

    new

--- nuxt3-app/pages/index.vue
+++ nuxt3-app/pages/index.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
+  <div class="main">
     <hr />
-    <h1></h1>
-    <button @click="handleClick">Coupon Get</button>
-    <LazyCoupon v-if="show" />
+    <h1><span>A</span> SYSTEMATIC</h1>
+    <h1>TO <span>ENGLISH</span> WORDS</h1>
   </div>
+  <P>分からないときは、passを入力すると<br />次の問題に移ります。</P>
 </template>
 
-<script setup>
-const show = ref(false);
-const handleClick = () => {
-  show.value = true;
-};
-</script>
+<script setup></script>
+
+<style scoped>
+.main {
+  margin-left: 1.5rem;
+}
+span {
+  color: rgb(229, 153, 166);
+}
+p {
+  margin-left: 1.5em;
+  color: rgb(41, 123, 155);
+  font-weight: bold;
+}
+</style>
